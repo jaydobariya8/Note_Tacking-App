52d5bbe673eb4f270d009f4209e4af511fe78645
jay <jaydobariya2341@gmail.com>
2022-07-18 10:52:44 +0530

    done with HTML

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("click",function(e){
+    
+    let addTxt = document.getElementById('addTxt');
+    let notes = localStorage.getItem("notes");
+
+    if(notes == null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value ="";
+    console.log(notesObj);
+})
